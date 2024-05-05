9126cb9cc025c08e90e6e1ddfae4022a04293953
nishantyadav24 <ynishant567@gmail.com>
2024-05-05 14:30:29 +0530

    first function based commit

--- NewsShore-main/src/components/Spinner.js
+++ NewsShore-main/src/components/Spinner.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import loading from './loading.gif'
 
-export default class Spinner extends Component {
-  render() {
+const Spinner =()=> {
+  
     return (
       <div className="text-center">
 
         <img src={loading} alt="loading" />
       </div>
     )
-  }
+  
+
 }
+export default Spinner
